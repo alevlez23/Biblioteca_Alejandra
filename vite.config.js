a052ca56db312c4eb803d8b6488b3fd5485062f9
Alejandra Velez <evelez7746@utm.edu.ec>
2026-08-20 22:16:36 -0500

    Corregir el formato de la ruta base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Configuración de Vite para GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: "/Biblioteca-Alejandra/",
-})
+
+  base: "/Biblioteca_Alejandra/",
+});
